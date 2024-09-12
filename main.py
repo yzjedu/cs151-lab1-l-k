16004b5c1d05c5c48a1c60dee53af912ef5b8b17
lelabianca <lelabianca@loyola.edu>
2024-09-12 13:32:02 -0400

    Python pt. 1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # Enter code here
 # Read the README for instructions.
 # You must write an algorithm and test cases first!
+price_per_gallon = 1
+distance_miles = int(input("Enter distance in miles: "))
+miles_per_gallon = int(input("Enter miles per gallon: "))
+price_of_trip = (distance_miles/miles_per_gallon)*price_per_gallon
+print("The total cost of the trip will be", price_of_trip)
